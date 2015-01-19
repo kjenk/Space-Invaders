ebb2ed173b88729d7ab7c3f37ce2c8d3c8b2396a
kjenk <kj@hntvlan569.1620.wlan.wireless-resnet.upenn.edu>
2015-01-19 02:18:44 -0500

    Revert 4b74ab2..5e20495
    This rolls back to commit 4b74ab2771244d954a2253f7afdc6cf47dd0f648.

--- Intersection.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * The results of an intersection test: no intersection, or the direction in
- * which the intersection happened.
- * 
- */
-public enum Intersection {
-    NONE, UP, LEFT, DOWN, RIGHT
-}
